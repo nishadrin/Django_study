76c0ade3ba15791ad9d9aea4610f62bfd610b4ea
nishadrin <nishadrin@gmail.com>
2019-04-10 23:36:56 +0300

    Lesson 2

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', mainapp.main),
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+]
+
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': "главная",
+    }
+
+    return render(request, 'mainapp/index.html', context)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': "продукты",
+        #'links_menu': links_menu,
+        #'same_products': same_products
+    }
+
+    return render(request, 'mainapp/products.html', context)
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': "контакты",
+    }
+
+    return render(request, 'mainapp/contact.html', context)
