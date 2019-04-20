6b660bf1ad85079c4a0414d4d590df0e3ea164ee
nishadrin <nishadrin@gmail.com>
2019-04-20 15:39:16 +0300

    Lesson 7

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,6 +7,7 @@ class ProductCategory(models.Model):
 
     name = models.CharField(verbose_name='имя', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание катергории', blank=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
 
     def __str__(self):
         return self.name
